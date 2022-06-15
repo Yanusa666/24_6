f2ea5bc45afcf6fdf5f3e435e77fe5a51a38bb6e
Yana Terletskaya <yterletskaya@mtt.ru>
2022-06-15 16:06:33 +0300

    добавлено возведение в степень

--- calc/calc.go
+++ calc/calc.go
@@ -1,39 +1,51 @@
 package calc
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type calculator struct {
-	}
+}
 
-func NewCalculator () *calculator {
+func NewCalculator() *calculator {
 	return new(calculator)
 }
 
-func (c *calculator) sum (first,second float64) float64{
-	result:=first+second
+func (c *calculator) sum(first, second float64) float64 {
+	result := first + second
 	return result
 }
 
-func (c *calculator) diff (first,second float64) float64{
-	result:=first-second
+func (c *calculator) diff(first, second float64) float64 {
+	result := first - second
 	return result
 }
 
-func (c *calculator) mult (first,second float64) float64{
-	result:=first*second
+func (c *calculator) mult(first, second float64) float64 {
+	result := first * second
 	return result
 }
 
-func (c *calculator) div (first,second float64) (result float64) {
+func (c *calculator) div(first, second float64) (result float64) {
 	if second == 0 {
 		fmt.Println("Делить на 0 нельзя")
 	} else {
 		result = first / second
-		}
+	}
 	return result
 }
 
-func (c *calculator) Calculate (num1, num2 float64, oper string) (result float64) {
+func (c *calculator) pow (first, power float64)  (result float64) {
+	if first < 0 && power<1 {
+		fmt.Println("Нельзя извлечь корень из отрицательного числа")
+	} else {
+		result = math.Pow(first, power)
+	}
+	return result
+}
+
+func (c *calculator) Calculate(num1, num2 float64, oper string) (result float64) {
 	switch oper {
 	case "+":
 		result = c.sum(num1, num2)
@@ -43,6 +55,8 @@ func (c *calculator) Calculate (num1, num2 float64, oper string) (result float64
 		result = c.mult(num1, num2)
 	case "/":
 		result = c.div(num1, num2)
+	case "^":
+		result = c.pow(num1, num2)
 	default:
 		fmt.Println("Невозможно выполнить данную операцию")
 	}
--- main.go
+++ main.go
@@ -15,8 +15,13 @@ func main() {
 	fmt.Scanln(&num1)
 	fmt.Print("Введите арифметический оператор:")
 	fmt.Scanln(&oper)
-	fmt.Print("Введите 2е число:")
-	fmt.Scanln(&num2)
+	if oper == "^" {
+		fmt.Print("Введите степень возведения:")
+		fmt.Scanln(&num2)
+	} else {
+		fmt.Print("Введите 2е число:")
+		fmt.Scanln(&num2)
+	}
 
 	calculator := calc.NewCalculator()
 	result := calculator.Calculate(num1, num2, oper)
